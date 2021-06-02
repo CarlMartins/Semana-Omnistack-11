fc01cf8533d6c8181475c6ff9a8a661a26fb73b4
Carlos Martins <lemos_oliveiraa@hotmail.com>
2021-06-02 11:03:28 -0300

    Construida parte visual do perfil da ONG

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import './styles.css';
+import logoImg from '../../assets/logo.svg';
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <header>
+        <img src={logoImg} alt="Be the Hero" />
+        <span>Bem vinda, APAD</span>
+        <Link to="incidents/new" className="button">Cadastrar novo caso</Link>
+        <button type="button">
+          <FiPower size={18} color="#E02041" />
+        </button>
+      </header>
+
+      <h1>Casos cadastrador</h1>
+      <ul>
+        <li>
+          <strong>CASO: </strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descricao teste</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button type='button'>
+            <FiTrash2 size={20} color="#a8a8b3" />
+          </button>
+        </li>
+        <li>
+          <strong>CASO: </strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descricao teste</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button type='button'>
+            <FiTrash2 size={20} color="#a8a8b3" />
+          </button>
+        </li>
+        <li>
+          <strong>CASO: </strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descricao teste</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button type='button'>
+            <FiTrash2 size={20} color="#a8a8b3" />
+          </button>
+        </li>
+        <li>
+          <strong>CASO: </strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descricao teste</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button type='button'>
+            <FiTrash2 size={20} color="#a8a8b3" />
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
